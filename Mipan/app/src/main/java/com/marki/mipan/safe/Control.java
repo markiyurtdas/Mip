8afb9156c7e5605de660bc953cb95b6a7600bfdf
iyurtdas <ikram.yurtdas@nucleo.com.tr>
2019-12-28 20:26:42 +0300

    getEvent

--- Mipan/app/src/main/java/com/marki/mipan/safe/Control.java
+++ Mipan/app/src/main/java/com/marki/mipan/safe/Control.java
@@ -18,6 +18,7 @@ import org.json.JSONObject;
 import java.security.Key;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Set;
 
 public class Control extends AppCompatActivity {
@@ -27,6 +28,9 @@ public class Control extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_control);
+
+        getEvents();
+
 
 
 
@@ -45,17 +49,31 @@ public class Control extends AppCompatActivity {
                             HashMap<String,Object> myHash = (HashMap<String, Object>) dataSnapshot.getValue();
                             Set<String> str = myHash.keySet();
                             for(String string:str){
-                                String o = myHash.get(string).toString();
-                                Event mEvent = gson.fromJson(o,Event.class);
-                                
+                                Event mEvent =  new Event();
+
+
+                                HashMap<String,Object> o = (HashMap<String, Object>) myHash.get(string);
+
+                                mEvent.setDate((Long) o.get("date"));
+                                mEvent.setDescription((String) o.get("description"));
+                                long id = (long) o.get("event_id");
+                                mEvent.setEvent_id(id);
+                                mEvent.setLocLat((Double) o.get("loc_lat"));
+                                mEvent.setLocLon((Double) o.get("loc_lon"));
+                                mEvent.setMip_coin((Long) o.get("mip_coin"));
+                                mEvent.setPhoto((String) o.get("description"));
+                                mEvent.setTitle((String) o.get("title"));
+                                eventList.add(mEvent);
                             }
-                            JSONObject o = (JSONObject) jArray.get(i);
-                            User tempUser = gson.fromJson(o.toString(), User.class);
-                            for (int j = 0; j < o.getJSONArray("create_courses").length(); j++) {
-                                Course c = gson.fromJson(o.getJSONArray("create_courses").get(j).toString(), Course.class);
-                                tempUser.addToCreateCourses(c);
-                                kurslar.put(c.getCourse_name(),c);
-                            }
+
+                            System.out.println("zxc event list"+eventList.toString());
+//                            JSONObject o = (JSONObject) jArray.get(i);
+//                            User tempUser = gson.fromJson(o.toString(), User.class);
+//                            for (int j = 0; j < o.getJSONArray("create_courses").length(); j++) {
+//                                Course c = gson.fromJson(o.getJSONArray("create_courses").get(j).toString(), Course.class);
+//                                tempUser.addToCreateCourses(c);
+//                                kurslar.put(c.getCourse_name(),c);
+//                            }
                         }
                     }
 
